be2dfab77f8928ac6a6d430b37d453ddf3d028ec
Nikolai Mushegian <mail@nikolai.fyi>
2022-06-12 15:32:53 -0400

    plug

--- core/plug.ts
+++ core/plug.ts
@@ -4,17 +4,26 @@ import { Mail } from './word.js'
 
 export { Plug }
 
+type Peer = string
+
 class Plug {
-    wss
+    socks
+    peers
     constructor(port :number) {
-        this.wss = new WebSocketServer({ port });
+        this.socks = new WebSocketServer({ port });
+        this.peers = {}
     }
-    when(what:((mail:Mail,back:((mail:Mail)=>void)) => void)) {
-        this.wss.on('connection', ws => {
-            // add to connections
-            ws.on(msg => {
-                what(msg, outs => {
-                    outs.map(out => ws.send(out))
+    when( what:( (mail:Mail,back:((mail:Mail)=>void) )=>void) )
+    {
+        this.socks.on('connection', sock => {
+            let peer = 'sock.url'
+            this.link(peer, sock)
+            sock.on(mail => {
+                what(mail, outs => {
+                    for (let out of outs) {
+                        this.drop(peer)
+                    }
+                    outs.map(out => sock.send(out))
                 })
             })
         })
@@ -23,4 +32,16 @@ class Plug {
         // for each connection, send it
     }
     async play() {}
+
+    // todo link/drop cleanup etc
+    link(peer :Peer, sock:any) {
+        this.peers[peer] = sock
+    }
+    drop(peer :Peer) {
+        let sock = this.peers[peer]
+        delete this.peers[peer]
+    }
+    pals() :Peer[] {
+        return Object.keys(this.peers)
+    }
 }
