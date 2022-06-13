ec918b7893c35013c22b71dadef3a0977a668774
Nikolai Mushegian <mail@nikolai.fyi>
2022-06-13 12:54:39 -0400

    tree

--- core/tree.ts
+++ core/tree.ts
@@ -35,52 +35,63 @@ class Tree {
     }
 
     // ['thin', tockhash] -> stat
-    view_thin(tosh :Mash) :Okay<Stat> { return fail(`todo`) }
+    read_thin(tosh :Mash) :Okay<Stat> { return fail(`todo`) }
     grow_thin(tosh :Mash, stat :Stat) { return fail(`todo`) }
 
     // ['know', tockhash] -> PV | DV | PN | DN
-    view_know(tosh :Mash) :Okay<Know> { return fail(`todo`) }
+    read_know(tosh :Mash) :Okay<Know> { return fail(`todo`) }
     grow_know(tosh :Mash, know :Know) { return fail(`todo`) }
 
     // ['part', tockhash, tackhash] -> snap
-    view_part(tosh :Mash, tash :Mash) :Okay<Snap> { return fail(`todo`) }
+    read_part(tosh :Mash, tash :Mash) :Okay<Snap> { return fail(`todo`) }
     grow_part(tosh :Mash, tash :Mash, snap :Snap) { return fail(`todo`) }
 
     // ['full', tockhash] -> snap
-    view_full(tosh :Mash) :Okay<Snap> { return fail(`todo`) }
+    read_full(tosh :Mash) :Okay<Snap> { return fail(`todo`) }
     grow_full(tosh :Mash, snap :Snap) { return fail(`todo`) }
 
     // ['page', snap] -> utxo -> [[hash,cash],burn]
-    view_page(copy :Snap, key :Blob) :Okay<[Bill,Bnum]> {
+    read_page(copy :Snap, key :Blob) :Okay<[Bill,Bnum]> {
         let snap = this._snaps[b2h(copy)]
         return pass([[], h2b(snap)])
     }
-    grow_page(copy :Snap, edit :((desk:{
+    grow_page(copy :Snap, edit :((_:{
         get: (key :Blob) => Blob;
         set: (key :Blob, val :Blob) => void;
     }) => void)) :Okay<Snap> {
-        let mut = this.desk //.mutableCopy
-        edit({
-            get: (k :Blob) :Blob => {
-                return (mut.get(k) as Blob)
-            },
-            set: (k :Blob, v :Blob) => {
-                mut.set(k, v)
-            }
+        let prev = this._snaps[b2h(copy)]
+        let next = prev.withMutations(page => {
+            edit({
+                get: (k :Blob) :Blob => {
+                    return page.get(k) as Blob
+                },
+                set: (k :Blob, v :Blob) => {
+                    page.set(k, v)
+                }
+            })
         })
-        // mut.seal()
         let snap = this._nextsnap()
-        this._snaps[snap] = mut
+        this._snaps[b2h(snap)] = next
         return pass(snap)
     }
 
-    _nextsnap() :number {
-        return this._snapc++
+    _nextsnap() :Snap {
+        let hexnum = (this._snapc++).toString(16)
+        let hexblob = blob(hexnum)
+        return hexblob
     }
 
-    //  not necessary, but useful -- info *about this branch*
-    //  page_ticks : snap -> (tickhash -> tockhash)  // early dup check
-    //  page_tocks : snap -> (tockhash -> height)    // faster common ancestor
+    //  not necessary, but useful:
+
+    //    early dup check
+    //      ['tickpage', snap] -> tickhash -> tockhash)
+    //      read_tickpage
+    //      grow_tickpage
+
+    //    fast common ancestor
+    //      ['tockpage', snap] -> tockhash -> height
+    //      read_tockpage
+    //      grow_tockpage
 
 }
 
