3756c2aee6fac80162b88984aebda165a640818b
kbrav <>
2022-06-19 13:26:36 -0400

    checksig test

--- test/vinx.t.ts
+++ test/vinx.t.ts
@@ -4,34 +4,44 @@ import {
     roll, h2b,
     sign, scry,
     Tick,
-    addr,
+    addr, t2b
 } from '../core/word.js'
+import {
+    form_tick
+} from '../core/well.js'
 
 import {
     _checksig,
     vinx_tick,
 } from '../core/vinx.js'
+import elliptic from 'elliptic'
+const ec = new elliptic.ec('secp256k1')
 
 test('checksig', t=>{ try {
-    let seck = h2b('00'.repeat(32))
-    let pubk = h2b('ff'.repeat(32)) // todo keypair
-    let code = addr(pubk) // in prev tick
+    const keypair = ec.genKeyPair()
+    const seck = keypair.getPrivate().toBuffer()
+    const pubk = Buffer.from(keypair.getPublic().encodeCompressed())
+    const code = addr(pubk) // in prev tick
 
     let tick = [[
-        ['aa'.repeat(24), '00', 'cc'.repeat(32)].map(h2b)
+        ['aa'.repeat(24), '00', 'cc'.repeat(65)].map(h2b)
     ],[
-        ['dd'.repeat(20), '0000ffffffffff'].map(h2b)
+        [code, '0000ffffffffff',].map(h2b)
     ]] as Tick
-    let mask = roll([[
-        ['aa'.repeat(24), '00', ''].map(h2b)
-    ],[
-        ['dd'.repeat(20), '0000ffffffffff'].map(h2b)
-    ]])
+    form_tick(tick)
+    const move = tick[0][0]
+    const ments = tick[1]
+    const mask = roll([
+        t2b("minicash movement"),
+        [ [ move[0], move[1], t2b('') ] ],
+        ments
+    ])
 
     let sig = sign(mask, seck)
 
     tick[0][0][2] = sig
 
+    form_tick(tick)
     let ok = _checksig(tick, 0, code)
     t.ok(ok, 'checksig must succeed')
 } catch (e) { t.ok(false, e.reason) }})
