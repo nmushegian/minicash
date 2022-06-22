3b1fa4e9af80c30f325fcab1e99b240308a9de63
Nikolai Mushegian <mail@nikolai.fyi>
2022-06-22 19:35:59 -0400

    lmdbrock

---
+++ core/lmdbrock.ts
@@ -0,0 +1,60 @@
+import { default as lmdb } from 'node-lmdb'
+
+import {
+    Blob,
+    need, toss, err
+} from './word.js'
+
+export {
+    LmdbRock, LmdbRite
+}
+
+class LmdbRite {
+    dbi
+    dbtx
+    constructor(dbi, dbtx) {
+        this.dbi = dbi
+        this.dbtx = dbtx
+    }
+    etch(key :Blob, val :Blob) {
+        // aver not set
+        this.dbtx.putBinary(this.dbi, key, val)
+    }
+    read(key :Blob) :Blob {
+        return this.dbtx.getBinary(this.dbi, key)
+    }
+}
+
+class LmdbRock {
+    env
+    dbi
+    constructor(path) {
+        this.env = new lmdb.Env()
+        this.env.open({ path })
+        this.dbi = this.env.openDbi({
+            name: "testdb",
+            keyIsBuffer: true,
+            create: true
+        })
+    }
+
+    load() {} // todo from constructor
+    save() {
+        this.dbi.close()
+        this.env.close()
+    }
+
+    rite(f:((LmdbRite)=>void)) {
+        let dbtx = this.env.beginTxn()
+        let rite = new LmdbRite(this.dbi, dbtx)
+        try {
+            f(rite)
+            //rite._seal()   todo check how nice lmdb plays with bad dbtx reuse
+            dbtx.commit()
+        } catch (e) {
+            //rite._seal()
+            dbtx.abort()
+            throw e
+        }
+    }
+}
