45ba3c31d8bb3312b8e69c57391bda18372101b2
Nikolai Mushegian <924397+nmushegian@users.noreply.github.com>
2022-07-13 11:28:17 -0400

    find_max (#42)
    * find_max
    
    * find_max
    
    * find_max
    
    * find_max

--- test/rock.t.ts
+++ test/rock.t.ts
@@ -42,8 +42,12 @@ test.only('rock', t=>{ try {
         r.etch(h2b('bb11'), h2b('0003'))
         r.etch(h2b('ccaa'), h2b('0004'))
         r.etch(h2b('ccbb'), h2b('0005'))
+        r.etch(h2b('dd'), h2b('0006'))
+        r.etch(h2b('dd00'), h2b('0007'))
+        r.etch(h2b('dd11'), h2b('0008'))
     })
 
+    // find_min
     rock.rite(r => {
         let [key,val] = r.find_min(h2b('bb'), 2)
         t.deepEqual(val, h2b('0002'))
@@ -51,10 +55,37 @@ test.only('rock', t=>{ try {
         let [key2, val2] = r.find_min(h2b('cc'), 2)
         t.deepEqual(val2, h2b('0004'))
 
-        let [key3, val3] = r.find_min(h2b('dd'), 2)
+        let [key3, val3] = r.find_min(h2b('ff'), 2)
         t.deepEqual(val3, h2b('')) // emptyblob initialized
+
+        let [k4, v4] = r.find_min(h2b('ccbb'), 2)
+        t.deepEqual(v4, h2b('0005'))
+
+        let [k5, v5] = r.find_min(h2b('aa00'), 2)
+        t.deepEqual(v5, h2b('0000'))
+
+        let [k6, v6] = r.find_min(h2b('dd'), 2)
+        t.deepEqual(v6, h2b('0007'))
+
     })
 
-    rock.shut()
+    // find_max
+    rock.rite(r => {
+        let [k1, v1] = r.find_max(h2b('cc'), 2)
+        t.deepEqual(v1, h2b('0005'))
+
+        let [k2, v2] = r.find_max(h2b('dd'), 2)
+        t.deepEqual(v2, h2b('0008'))
+
+        let [k3, v3] = r.find_max(h2b('dd11'), 2)
+        t.deepEqual(v3, h2b('0008'))
+
+        let [k4, v4] = r.find_max(h2b('aa'), 2)
+        t.deepEqual(v4, h2b('0001'))
+
+        let [k5, v5] = r.find_max(h2b('aa00'), 2)
+        t.deepEqual(v5, h2b('0000'))
+    })
+
 } catch (e) { t.ifError(e, 'rock test throw') } } )
 
