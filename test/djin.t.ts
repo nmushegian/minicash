86ee97c24b9892f802df2be757184947da145ac6
Nikolai Mushegian <mail@nikolai.fyi>
2022-07-16 10:09:36 -0400

    canon

--- test/djin.t.ts
+++ test/djin.t.ts
@@ -1,5 +1,5 @@
 import Debug from 'debug'
-const dub = Debug('cash:djin')
+const dub = Debug('cash:djin:test')
 
 import { test } from 'tapzero'
 import { jams } from 'jams.js'
@@ -19,36 +19,47 @@ import {
 
 import { readdirSync, readFileSync } from "fs";
 
-const runcase = (dir, name) => {
+const runcase = (dir, name, t) => {
     if (!name.endsWith('.jams')) return
-    test(`${name}`, t => {
-        dub(`TESTING: ${dir + name}`)
-        let djin = new Djin('./test/db', true)
-        let path = dir + name
-        let file = readFileSync(path)
-        let data = jams(file.toString())
-        let out
-        for (let cmd of data) {
-            let [func, memohex] = cmd
-            let memo = rmap(memohex, h2b)
-            if ('send' == func) {
-                out = djin.turn(memo)
-                continue
-            }
-            if ('want' == func) {
-                t.deepEqual(memo, out)
-                continue
-            }
-            throw err(`unrecognized test command`)
+    dub(`TESTING: ${dir + name}`)
+    let djin = new Djin('./test/db', true)
+    let path = dir + name
+    let file = readFileSync(path)
+    let data = jams(file.toString())
+    let out
+    for (let cmd of data) {
+        let [func, memohex] = cmd
+        let memo = rmap(memohex, h2b)
+        if ('send' == func) {
+            out = djin.turn(memo)
+            continue
         }
-        djin.kill()
-    })
+        if ('want' == func) {
+            if (!bleq(roll(memo), roll(out))) {
+                dub('want failed')
+                dub('want', memo)
+                dub('got ', out)
+            }
+            t.deepEqual(memo, out)
+            continue
+        }
+        if ('note' == func) {
+            continue
+        }
+        throw err(`unrecognized test command`)
+    }
+    djin.kill()
 }
 
 test('djin', t=>{
     let dir = './test/case/djin/'
     let cases = readdirSync(dir)
 
-    cases.forEach(c => runcase(dir, c))
+    cases.forEach(c => test(c, t=> {
+        runcase(dir, c, t)
+    }))
 })
 
+test.only('canon', t=> {
+    runcase('./test/case/djin/', 'canon.jams', t)
+})
