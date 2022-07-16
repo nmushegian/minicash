58829fffa36796a0eeed7216e05afeddc1d7ab55
Nikolai Mushegian <mail@nikolai.fyi>
2022-07-15 21:16:40 -0400

    more tree tests

--- test/tree.t.ts
+++ test/tree.t.ts
@@ -73,6 +73,7 @@ test('tree2', t=>{
 
     let set1 = {}
     let set2 = {}
+    let set3 = {}
     for (let i = 0; i < 10; i++) {
         let k1 = randomBytes(tree.keysize)
         let v1 = randomBytes(12)
@@ -81,6 +82,10 @@ test('tree2', t=>{
         let k2 = randomBytes(tree.keysize)
         let v2 = randomBytes(12)
         set2[b2h(k2)] = v2
+
+        let k3 = randomBytes(tree.keysize)
+        let v3 = randomBytes(12)
+        set3[b2h(k3)] = v3
     }
 
     let inner1 // sanity check
@@ -104,20 +109,79 @@ test('tree2', t=>{
         }
     })
 
+    // snap2 has set1 and set2
     tree.look(snap2, (rock, twig) => {
+        for (let [k, v] of Object.entries(set1)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
         for (let [k, v] of Object.entries(set2)) {
             let got = twig.read(h2b(k))
             t.deepEqual(got, v)
         }
     })
 
-    // snap1 has all originals, but none of the new ones
+    // snap1 has set1, and not set2
     tree.look(snap1, (rock, twig) => {
         for (let [k, v] of Object.entries(set1)) {
             let got = twig.read(h2b(k))
             t.deepEqual(got, v)
         }
         for (let [k, v] of Object.entries(set2)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, h2b(''))
+        }
+    })
+
+    let snap3 = tree.grow(snap2, (rock, twig, snap) => {
+        for (let [k, v] of Object.entries(set3)) {
+            twig.etch(h2b(k), v)
+        }
+    })
+
+    // snap3 has set1, set2, and set3
+    tree.look(snap3, (rock, twig) => {
+        for (let [k, v] of Object.entries(set1)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+        for (let [k, v] of Object.entries(set2)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+        for (let [k, v] of Object.entries(set3)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+    })
+
+    // snap1 has set1, and not set2 or set3
+    tree.look(snap1, (rock, twig) => {
+        for (let [k, v] of Object.entries(set1)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+        for (let [k, v] of Object.entries(set2)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, h2b(''))
+        }
+        for (let [k, v] of Object.entries(set3)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, h2b(''))
+        }
+    })
+
+    // snap2 has set1 and set2, but not set3
+    tree.look(snap2, (rock, twig) => {
+        for (let [k, v] of Object.entries(set1)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+        for (let [k, v] of Object.entries(set2)) {
+            let got = twig.read(h2b(k))
+            t.deepEqual(got, v)
+        }
+        for (let [k, v] of Object.entries(set3)) {
             let got = twig.read(h2b(k))
             t.deepEqual(got, h2b(''))
         }
@@ -155,17 +219,4 @@ test('tree3', t=>{
 
     rock.shut()
 
-
-        /*
-    691edf2cd9b9eb6e5e0625ad840a5b4d55a31d188240a7778cb50ca01b a6bac3d17737e53578ae4405
-
-    0ef1d80b8fb222948a6cfe6c9f281c612e3f23d3e5d097d9938a42ca02 c264fa70b1a29fd01b7a6204
-    e46aea12389f5c679d58d2b2c8ca015ffc6fdc74f07240489783b31699 1602b3cefa329207fb75ca7a
-    a5ec27c91e248a0232ce5d0341a00c37892587c03394d031f17ef7497b 1b4fb72559ea63f577bc4358
-    3af2bfe1a116594c3877dc6fd88837b53da698ff953c22ef35dab62d40 2a8cf4860cbcd6fc990ff6c5
-    6b0804cc197405d15a225b9138c61f2c498aa9dec94591536fbf6540fd 692a7b33226eecddc4d82aa2
-    eea29bb6c087bd1b7785ff24ea08402450334c407e61e4ed2b4586171e c51bcf99ab0fb20cd5e2c53f
-    d9d5f255cfb4b66a224e5f942caa91a631bcd0abe8ea2fd00d801369bb 4df62c9d6a1826147fb6fcdb
-
-    */
 })
